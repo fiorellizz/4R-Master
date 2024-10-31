38054e59522450ec76104e9c595a67e82b26b52f
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2024-10-31 16:27:47 -0300

    Versao Desenvolvimento

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fidk&q&n9z1ck%qk_&-^9vlf9p!wj_3!y=tm3&1k_uam4a#z80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['4rmaster.com', '92.113.34.52', 'localhost']
+ALLOWED_HOSTS = ['*',]
 
 # Application definition
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
